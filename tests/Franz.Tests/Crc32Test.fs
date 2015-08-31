305e007c6e8f4acdd09b72af4b88f85095bedabc
Kim Christensen <kich@mvno.dk>
2015-08-31 22:05:05 +0200

    Add test for Crc32

---
+++ tests/Franz.Tests/Crc32Test.fs
@@ -0,0 +1,12 @@
+﻿module Crc32Test
+
+open Xunit
+open Franz.Internal
+open System.Text
+open Swensen.Unquote
+
+type Crc32Test() =
+    [<Fact>]
+    member __.``CRC32 is calculated correctly`` () =
+        let value = Encoding.UTF8.GetBytes("Test")
+        test <@ Crc32.crc32(value) = 0x784dd132 @>
