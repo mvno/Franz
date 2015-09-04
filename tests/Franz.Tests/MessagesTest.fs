0d383cf434268c3ad2873fcca50af6c66be67f4e
Kim Christensen <kich@mvno.dk>
2015-09-04 10:13:54 +0200

    Add tests for messageset

--- tests/Franz.Tests/MessagesTest.fs
+++ tests/Franz.Tests/MessagesTest.fs
@@ -12,6 +12,10 @@ module Helpers =
         if BitConverter.IsLittleEndian then
             array |> Array.Reverse
         BitConverter.ToInt32(array, 0)
+    let convertToInt64 array =
+        if BitConverter.IsLittleEndian then
+            array |> Array.Reverse
+        BitConverter.ToInt64(array, 0)
     let convertToInt16 array =
         if BitConverter.IsLittleEndian then
             array |> Array.Reverse
@@ -102,4 +106,70 @@ type RequestBaseTest() =
                 && clientIdLength = (int16 5)
                 && clientIdValue = "Franz"
                 && content = requestBase.Content
+            @>
+
+type MessageSetTest() =
+    [<Fact>]
+    member __.``creating a message set sets the offset correctly`` () =
+        let offset = int64 100
+        let messageSet = MessageSet.Create(offset, int8 0, [||], [||])
+        test <@ messageSet.Offset = offset @>
+
+    [<Fact>]
+    member __.``creating a message set sets the size correctly`` () =
+        let expectedMessageSize = 14
+        let messageSet = MessageSet.Create(int64 0, int8 0, [||], [||])
+        test <@ messageSet.MessageSize = expectedMessageSize @>
+
+    [<Fact>]
+    member __.``creating a message set sets the message correctly`` () =
+        let attributes = int8 1
+        let key = Encoding.UTF8.GetBytes("Key")
+        let value = Encoding.UTF8.GetBytes("Value")
+        let messageSet = MessageSet.Create(int64 0, attributes, key, value)
+        let message = messageSet.Message
+        let expectedMagicByte = int8 0
+        test
+            <@
+                message.Attributes = attributes
+                && message.MagicByte = expectedMagicByte
+                && message.Key |> Seq.forall2 (fun x y -> x = y) key
+                && message.Value |> Seq.forall2 (fun x y -> x = y) value
+            @>
+
+    [<Fact>]
+    member __.``message set is serialized correctly`` () =
+        let stream = new MemoryStream()
+        let offset = int64 100
+        let messageSize = 120
+        let magicByte = int8 0
+        let attributes = int8 0
+        let key = Encoding.UTF8.GetBytes("Key")
+        let value = Encoding.UTF8.GetBytes("Value")
+        let crc = 0
+        let messageSet = new MessageSet(offset, messageSize, { Crc = crc; MagicByte = magicByte; Attributes = attributes; Key = key; Value = value })
+        messageSet.Serialize(stream)
+        stream.Position <- int64 0
+        let buffer = Array.zeroCreate(stream.Length |> int32)
+        stream.Read(buffer, 0, stream.Length |> int32) |> ignore
+        let actualOffset = buffer |> Array.take(8) |> Helpers.convertToInt64
+        let actualMessageSize = buffer |> Array.skip(8) |> Array.take(4) |> Helpers.convertToInt32
+        let actualCrc = buffer |> Array.skip(12) |> Array.take(4) |> Helpers.convertToInt32
+        let actualMagicByte = buffer |> Array.skip(16) |> Array.take(1) |> Array.head
+        let actualAttributes = buffer |> Array.skip(17) |> Array.take(1) |> Array.head
+        let actualKeyLength = buffer |> Array.skip(18) |> Array.take(4) |> Helpers.convertToInt32
+        let actualKeyValue = buffer |> Array.skip(22) |> Array.take(actualKeyLength)
+        let actualValueLength = buffer |> Array.skip(22 + actualKeyLength) |> Array.take(4) |> Helpers.convertToInt32
+        let actualValueValue = buffer |> Array.skip(22 + actualKeyLength + 4) |> Array.take(actualValueLength)
+        let expectedMagicByte = byte magicByte
+        let expectedAttributes = byte attributes
+        test
+            <@
+                actualOffset = offset
+                && actualMessageSize = messageSize
+                && actualCrc = crc
+                && actualMagicByte = expectedMagicByte
+                && actualAttributes = expectedAttributes
+                && actualKeyValue = key
+                && actualValueValue = value
             @>
