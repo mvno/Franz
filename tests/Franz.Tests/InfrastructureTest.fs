a7cf9e0626bed015368527ad5959510700b0773e
Kim Christensen <kich@mvno.dk>
2015-09-08 14:17:14 +0200

    Add missing attribute

--- tests/Franz.Tests/InfrastructureTest.fs
+++ tests/Franz.Tests/InfrastructureTest.fs
@@ -5,24 +5,28 @@ open Xunit
 open Swensen.Unquote
 
 type InternalTests() =
+    [<Fact>]
     member __.``roundRobin on single element list returns first element when the last index is 0`` () =
         let list = [1]
         let lastIndex = 0;
         let (_, element) = Seq.roundRobin lastIndex list
         test <@ element = (list |> Seq.head) @>
 
+    [<Fact>]
     member __.``roundRobin on single element list returns first element when the last index is greater than 0`` () =
         let list = [1]
         let lastIndex = 10;
         let (_, element) = Seq.roundRobin lastIndex list
         test <@ element = (list |> Seq.head) @>
 
+    [<Fact>]
     member __.``roundRobin on single element list returns new position as 0`` () =
         let list = [1]
         let lastIndex = 10;
         let (newPosition, _) = Seq.roundRobin lastIndex list
         test <@ newPosition = 0 @>
 
+    [<Fact>]
     member __.``roundRobin returns second element when last position is 1`` () =
         let list = [1; 2]
         let lastPostion = 1
@@ -32,6 +36,7 @@ type InternalTests() =
                 element = Seq.last list
             @>
 
+    [<Fact>]
     member __.``roundRobin returns next position as last position plus one`` () =
         let list = [1; 2]
         let lastPostion = 2
