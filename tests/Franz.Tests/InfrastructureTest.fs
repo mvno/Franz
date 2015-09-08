f9cd23a7ae420d853a2f138b66252bd49d521eb1
Kim Christensen <kich@mvno.dk>
2015-09-08 14:16:39 +0200

    REVIEW: Fix test naming

--- tests/Franz.Tests/InfrastructureTest.fs
+++ tests/Franz.Tests/InfrastructureTest.fs
@@ -11,7 +11,7 @@ type InternalTests() =
         let (_, element) = Seq.roundRobin lastIndex list
         test <@ element = (list |> Seq.head) @>
 
-    member __.``roundRobin on single element list returns first element whenthe last index is greater than 0`` () =
+    member __.``roundRobin on single element list returns first element when the last index is greater than 0`` () =
         let list = [1]
         let lastIndex = 10;
         let (_, element) = Seq.roundRobin lastIndex list
