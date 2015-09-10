ba1131230e0b89f6051a7cc2bd2fd68e1b765cf4
Kim Christensen <kich@mvno.dk>
2015-09-10 22:31:15 +0200

    Bump version to 1.0.0

--- src/Franz/AssemblyInfo.fs
+++ src/Franz/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("Franz")>]
 [<assembly: AssemblyProductAttribute("Franz")>]
 [<assembly: AssemblyDescriptionAttribute("Kafka client for .NET")>]
-[<assembly: AssemblyVersionAttribute("0.0.4")>]
-[<assembly: AssemblyFileVersionAttribute("0.0.4")>]
+[<assembly: AssemblyVersionAttribute("1.0.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0.0")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.0.4"
+    let [<Literal>] Version = "1.0.0"
