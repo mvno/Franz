89ddb3c59f5062d4c322c591d9155e3bb8ded63f
Christian Mikkelsen <chmi@mvno.dk>
2016-04-04 11:02:34 +0200

    Task Unqoute

--- tests/Franz.Tests/ConsumerOptionsTest.fs
+++ tests/Franz.Tests/ConsumerOptionsTest.fs
@@ -2,16 +2,17 @@ namespace Franz.Tests
 
 open Franz.HighLevel
 open Xunit
+open Swensen.Unquote
 
 type ConsumerOptionsTest() =
 
     [<Fact>]
     member __.``default offsetstorage value is Zookeeper`` () =
         let options = new ConsumerOptions()
-        Assert.Equal(options.OffsetStorage, OffsetStorage.Zookeeper)
+        test <@ options.OffsetStorage = OffsetStorage.Zookeeper @>
 
     [<Fact>]
     member __.``setting a offsetstorage value different than the default value is possible`` () =
         let options = new ConsumerOptions()
         options.OffsetStorage <- OffsetStorage.DualCommit
-        Assert.Equal(options.OffsetStorage, OffsetStorage.DualCommit)
+        test <@ options.OffsetStorage = OffsetStorage.DualCommit @>
