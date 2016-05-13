fc26bc3d35d1299d12f494112bab555a507e2e9c
Kim Christensen <kich@mvno.dk>
2016-05-13 16:40:22 +0200

    Basic TcpClientWrapper

---
+++ src/Franz/TcpClient.fs
@@ -0,0 +1,34 @@
+﻿namespace Franz.Internal
+
+[<RequireQualifiedAccess>]
+module TcpClient =
+    open System.IO
+    open System.Net.Sockets
+
+    type TcpState = | NotConnected | Connected
+    type T = { Client : TcpClient; State : TcpState; Stream : Stream option; Host : string; Port : int }
+    let create() = { Client = new TcpClient(); State = NotConnected; Stream = None; Host = null; Port = 0 }
+    let isConnected client = client.State = Connected
+    let isDisconnected client = isConnected client |> not
+    let connectTo (host : string) (port : int) (client : T) =
+        if client |> isConnected then invalidOp "Already connected"
+        client.Client.Connect(host, port)
+        { client with Stream = Some (client.Client.GetStream() :> Stream); State = Connected; Host = host; Port = port }
+    let write buffer client =
+        if client |> isDisconnected then invalidOp "Not connected"
+        client.Stream.Value.Write(buffer, 0, buffer.Length)
+        client
+    let readToBuffer numberOfBytesToRead client =
+        if client |> isDisconnected then invalidOp "Not connected"
+        if numberOfBytesToRead < 1 then invalidOp "Must read more than zero bytes"
+        let buffer = Array.zeroCreate(numberOfBytesToRead)
+        let bytesRead = client.Stream.Value.Read(buffer, 0, buffer.Length)
+        if bytesRead = 0 then invalidOp "Connection lost"
+        buffer
+    let read f client =
+        if client |> isDisconnected then invalidOp "Not connected"
+        client.Stream.Value |> f
+    let reconnect client =
+        if client |> isConnected then invalidOp "Already connected"
+        let newClient = { client with Client = new TcpClient(); Stream = None }
+        newClient |> connectTo client.Host client.Port
