e79f9591476813c9efb5312a5a3e7e7d2a41ca0e
Christian Mikkelsen <chmi@mvno.dk>
2016-06-14 21:09:27 +0200

    Added first proof of concept franz test

---
+++ tests/Franz.Integration.Tests/BasicTest.fs
@@ -0,0 +1,21 @@
+﻿module BasicTest
+
+open Xunit
+open Swensen.Unquote
+open Cluster
+open Franz
+open Franz.HighLevel
+
+let topicName = "Franz.Integration.Test"
+
+[<FranzFact>]
+let ``must produce and consume 1 message`` () =
+    let broker = new BrokerRouter(kafka_brokers, 10000)
+    let producer = new RoundRobinProducer(broker)
+    let consumer = new ChunkedConsumer(kafka_brokers, topicName)
+    let expectedMessage = "must produce and consume 1 message"
+    producer.SendMessages(topicName, null, [|expectedMessage|]);
+
+    let message = consumer.Consume() |> Seq.head
+
+    Assert.Equal(expectedMessage, System.Text.Encoding.UTF8.GetString(message.Message.Value))
