59b08a14c0dbbddf3190c1fde7aec79246ef1678
Christian Mikkelsen <chmi@mvno.dk>
2016-06-15 09:18:40 +0200

    Reorder since creation time of consumer is important

--- tests/Franz.Integration.Tests/BasicTest.fs
+++ tests/Franz.Integration.Tests/BasicTest.fs
@@ -12,9 +12,9 @@ let topicName = "Franz.Integration.Test"
 let ``must produce and consume 1 message`` () =
     let broker = new BrokerRouter(kafka_brokers, 10000)
     let producer = new RoundRobinProducer(broker)
-    let consumer = new ChunkedConsumer(kafka_brokers, topicName)
     let expectedMessage = "must produce and consume 1 message"
     producer.SendMessages(topicName, null, [|expectedMessage|]);
+    let consumer = new ChunkedConsumer(kafka_brokers, topicName)
 
     let message = consumer.Consume() |> Seq.head
 
