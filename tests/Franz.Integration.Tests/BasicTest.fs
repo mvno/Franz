7992def953dde5bdead5da3e8c2e64e486c41450
Christian Mikkelsen <chmi@mvno.dk>
2016-09-15 21:38:09 +0200

    Fix test

--- tests/Franz.Integration.Tests/BasicTest.fs
+++ tests/Franz.Integration.Tests/BasicTest.fs
@@ -5,6 +5,7 @@ open Swensen.Unquote
 open Cluster
 open Franz
 open Franz.HighLevel
+open System.Threading
 
 let topicName = "Franz.Integration.Test"
 
@@ -12,10 +13,11 @@ let topicName = "Franz.Integration.Test"
 let ``must produce and consume 1 message`` () =
     let broker = new BrokerRouter(kafka_brokers, 10000)
     let producer = new RoundRobinProducer(broker)
-    let expectedMessage = "must produce and consume 1 message"
-    producer.SendMessages(topicName, null, [|expectedMessage|]);
+    let expectedMessage = {Key = ""; Value = "must produce and consume 1 message"}
+    producer.SendMessage(topicName, expectedMessage);
     let consumer = new ChunkedConsumer(kafka_brokers, topicName)
+    let token = new CancellationToken()
 
-    let message = consumer.Consume() |> Seq.head
+    let message = consumer.Consume(token) |> Seq.head
 
-    Assert.Equal(expectedMessage, System.Text.Encoding.UTF8.GetString(message.Message.Value))
+    Assert.Equal(expectedMessage.Value, System.Text.Encoding.UTF8.GetString(message.Message.Value))
