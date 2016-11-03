a6c3a17d76e17fb9d8de3268a584cfb56d61bd2b
Kim Christensen <kich@mvno.dk>
2016-11-03 22:28:00 +0100

    Add first test of consumer groups

---
+++ tests/Franz.Integration.Tests/ConsumerGroupTest.fs
@@ -0,0 +1,28 @@
+﻿module ConsumerGroupTest
+
+open Xunit
+open Swensen.Unquote
+open Cluster
+open Franz
+open Franz.HighLevel
+open System.Threading
+
+let topicName = "Franz.Integration.Test"
+
+[<FranzFact>]
+let ``consumer group consumer must be able to read 1 message`` () =
+    reset()
+
+    let broker = new BrokerRouter(kafka_brokers, 10000)
+    let producer = new RoundRobinProducer(broker)
+    let expectedMessage = {Key = ""; Value = "must produce and consume 1 message"}
+    producer.SendMessage(topicName, expectedMessage);
+    let options = new GroupConsumerOptions()
+    options.Topic <- topicName
+    let consumer = new GroupConsumer(broker, options)
+    let tokenSource = new CancellationTokenSource()
+
+    tokenSource.CancelAfter(30000)
+
+    let message = consumer.Consume(tokenSource.Token) |> Seq.tryHead
+    Assert.True(message.IsSome)
