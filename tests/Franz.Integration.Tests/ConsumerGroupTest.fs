4e72e19981753f3c4d272118cddc82639842de8c
Kim Christensen <kich@mvno.dk>
2016-11-04 22:38:04 +0100

    Add test for receiving message after startup

--- tests/Franz.Integration.Tests/ConsumerGroupTest.fs
+++ tests/Franz.Integration.Tests/ConsumerGroupTest.fs
@@ -30,3 +30,26 @@ let ``consumer group consumer must be able to read 1 message`` () =
             | Some x -> System.Text.Encoding.UTF8.GetString(x.Message.Value) = expectedMessage.Value
             | None -> false
         @>
+
+[<FranzFact>]
+let ``consumer group consumer must be able to read message after starting`` () =
+    reset()
+
+    let broker = new BrokerRouter(kafka_brokers, 10000)
+    let producer = new RoundRobinProducer(broker)
+    let expectedMessage = {Key = ""; Value = "must produce and consume 1 message"}
+    let options = new GroupConsumerOptions()
+    options.Topic <- topicName
+    let consumer = new GroupConsumer(broker, options)
+    let tokenSource = new CancellationTokenSource()
+    let resetEvent = new ManualResetEvent(false)
+
+    async {
+        let message = consumer.Consume(tokenSource.Token) |> Seq.head
+        resetEvent.Set() |> ignore
+        test <@ System.Text.Encoding.UTF8.GetString(message.Message.Value) = expectedMessage.Value @>
+    } |> Async.Start
+
+    producer.SendMessage(topicName, expectedMessage);
+
+    resetEvent.WaitOne(30000)
