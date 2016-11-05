078ae0108d36babcf7952b6db9f64d011245ae3a
Kim Christensen <kich@mvno.dk>
2016-11-05 21:17:25 +0100

    Add tests for partition assignment

--- tests/Franz.Integration.Tests/ConsumerGroupTest.fs
+++ tests/Franz.Integration.Tests/ConsumerGroupTest.fs
@@ -55,3 +55,63 @@ let ``consumer group consumer must be able to read message after starting`` () =
     test<@ resetEvent.WaitOne(30000) @>
 
     tokenSource.Cancel()
+
+[<FranzFact>]
+let ``with one consumer all partitions is assigned to the consumer`` () =
+    reset()
+    createTopic topicName 3 3
+
+    let broker = new BrokerRouter(kafka_brokers, 10000)
+    let options = new GroupConsumerOptions()
+    options.Topic <- topicName
+    let consumer = new GroupConsumer(broker, options)
+    let tokenSource = new CancellationTokenSource()
+    let completedEvent = new ManualResetEvent(false)
+
+    async {
+        consumer.Consume(tokenSource.Token) |> ignore
+    } |> Async.Start
+
+    use s = consumer.OnConnected.Subscribe(fun x ->
+        let assignment = x.Assignment.PartitionAssignment |> Seq.head
+        let availablePartitionIds = broker.GetAvailablePartitionIds(topicName)
+        test <@ assignment.Topic = topicName && assignment.Partitions |> Seq.exists (fun x -> availablePartitionIds |> Seq.contains x) @>
+        completedEvent.Set() |> ignore)
+
+    test <@ completedEvent.WaitOne(30000) @>
+
+[<FranzFact>]
+let ``with two consumers partitions are split among them`` () =
+    reset()
+    createTopic topicName 2 2
+
+    let broker = new BrokerRouter(kafka_brokers, 10000)
+    broker.Connect()
+    let availablePartitionIds = broker.GetAvailablePartitionIds(topicName)
+    let options = new GroupConsumerOptions()
+    options.Topic <- topicName
+    options.SessionTimeout <- 10000
+    let consumer1 = new GroupConsumer(broker, options)
+    let consumer2 = new GroupConsumer(kafka_brokers, options)
+    let tokenSource = new CancellationTokenSource()
+    let completedEvent = new ManualResetEvent(false)
+
+    async {
+        consumer1.Consume(tokenSource.Token) |> ignore
+    } |> Async.Start
+
+    async {
+        consumer2.Consume(tokenSource.Token) |> ignore
+    } |> Async.Start
+
+    use s = consumer2.OnConnected.Subscribe(fun x ->
+        let assignment = x.Assignment.PartitionAssignment |> Seq.head
+        if assignment.Partitions |> Seq.length = 1 then
+            test
+                <@
+                    let partitionId = assignment.Partitions |> Seq.exactlyOne
+                    assignment.Topic = topicName && availablePartitionIds |> Seq.contains partitionId
+                @>
+            completedEvent.Set() |> ignore)
+
+    test <@ completedEvent.WaitOne(60000) @>
