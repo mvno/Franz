21e52e6c5d4b84ce6fbf43fe59f997b228268673
Kim Christensen <kich@mvno.dk>
2016-12-10 00:51:12 +0100

    Add test for consumer recovery

--- tests/Franz.Integration.Tests/RecoveryTest.fs
+++ tests/Franz.Integration.Tests/RecoveryTest.fs
@@ -5,6 +5,8 @@ open Cluster
 open Franz
 open Franz.HighLevel
 open System.Threading
+open Franz.Zookeeper
+open Franz.Internal
 
 let topicName = "Franz.Integration.Test"
 
@@ -12,7 +14,7 @@ let topicName = "Franz.Integration.Test"
 let ``producer can handle rolling cluster restart``() =
     reset()
     
-    let producer = new RoundRobinProducer(kafka_brokers)
+    use producer = new RoundRobinProducer(kafka_brokers)
     let message = { Key = ""; Value = "Test" }
     producer.SendMessage(topicName, message)
 
@@ -24,3 +26,30 @@ let ``producer can handle rolling cluster restart``() =
     start_a_kafka 3
 
     producer.SendMessage(topicName, message)
+
+[<FranzFact>]
+let ``consumer can handle rolling cluster restart``() =
+    reset()
+
+    use producer = new RoundRobinProducer(kafka_brokers)
+    let message = { Key = ""; Value = "Test" }
+    producer.SendMessage(topicName, message)
+
+    let options = new ConsumerOptions()
+    options.Topic <- topicName
+    use consumer = new ChunkedConsumer(kafka_brokers, options)
+    let cts = new CancellationTokenSource()
+    let msgs = consumer.Consume(cts.Token)
+    test <@ msgs |> Seq.length = 1 @>
+
+    shutdown_a_kafka 1
+    start_a_kafka 1
+    shutdown_a_kafka 2
+    start_a_kafka 2
+    shutdown_a_kafka 3
+    start_a_kafka 3
+    
+    producer.SendMessage(topicName, message)
+
+    let msgs = consumer.Consume(cts.Token)
+    test <@ msgs |> Seq.length = 1 @>
