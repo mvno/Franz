82f9490c9bc3a1ac0301ef03a6b5f777c4d64c2b
Kim Christensen <kich@mvno.dk>
2016-12-13 22:05:38 +0100

    Add test to ensure consumer can handle leader change

--- tests/Franz.Integration.Tests/RecoveryTest.fs
+++ tests/Franz.Integration.Tests/RecoveryTest.fs
@@ -53,3 +53,27 @@ let ``consumer can handle rolling cluster restart``() =
 
     let msgs = consumer.Consume(cts.Token)
     test <@ msgs |> Seq.length = 1 @>
+
+[<FranzFact>]
+let ``consumer can handle leader change``() =
+    reset()
+    createTopic topicName 3 3
+
+    let brokerRouter = new BrokerRouter(kafka_brokers, 10000)
+    brokerRouter.Connect()
+    use producer = new RoundRobinProducer(brokerRouter) :> IProducer
+    let message = { Key = ""; Value = "Test" }
+    producer.SendMessage(topicName, message, RequiredAcks.AllReplicas, 500)
+    producer.SendMessage(topicName, message, RequiredAcks.AllReplicas, 500)
+    producer.SendMessage(topicName, message, RequiredAcks.AllReplicas, 500)
+    let leaderForPartition0 = brokerRouter.GetBroker(topicName, 0)
+
+    let options = new ConsumerOptions()
+    options.Topic <- topicName
+    use consumer = new ChunkedConsumer(kafka_brokers, options)
+    let cts = new CancellationTokenSource()
+
+    shutdown_a_kafka leaderForPartition0.Id
+
+    let msgs = consumer.Consume(cts.Token)
+    test <@ msgs |> Seq.length = 3 @>
